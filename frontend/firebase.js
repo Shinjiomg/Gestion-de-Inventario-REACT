3e84af1cff5813f09876d4af059ea5f76d615f27
Shinjiomg <yonkitas9@gmail.com>
2024-02-08 00:45:11 -0500

    firebase autenticación

---
+++ frontend/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+    apiKey: "AIzaSyDFAhvp0q3N8pv0hpB7l4iS2_0-As0QPGQ",
+    authDomain: "sistema-de-inventario-7d186.firebaseapp.com",
+    databaseURL: "https://sistema-de-inventario-7d186-default-rtdb.firebaseio.com",
+    projectId: "sistema-de-inventario-7d186",
+    storageBucket: "sistema-de-inventario-7d186.appspot.com",
+    messagingSenderId: "648915339926",
+    appId: "1:648915339926:web:6400a657b07009762510b7",
+    measurementId: "G-B1NFMBW57Y"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export default app;
+
+
--- frontend/src/components/user-login.jsx
+++ frontend/src/components/user-login.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import app from '../../firebase'
+
 export default function UserLogin() {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const message = 'Las credenciales no son válidas'
+    const [loading, setLoading] = useState(false);
+
+    const handleSignIn = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        try {
+            const auth = getAuth(app);
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            console.log(userCredential.user);
+            navigate('/register');
+        } catch (error) {
+            setError(message);
+        } finally {
+            setLoading(false);
+        }
+    };
     return (
         <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -9,7 +33,7 @@ export default function UserLogin() {
                 </h1>
             </div>
             <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form className="space-y-6" method='post'>
+                <form className="space-y-6" method='post' onSubmit={handleSignIn}>
                     <div>
                         <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                             Correo electrónico
@@ -21,6 +45,8 @@ export default function UserLogin() {
                                 type="email"
                                 autoComplete="email"
                                 required
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
@@ -43,6 +69,8 @@ export default function UserLogin() {
                                 type="password"
                                 autoComplete="current-password"
                                 required
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
@@ -51,10 +79,12 @@ export default function UserLogin() {
                         <button
                             type="submit"
                             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600"
+                            disabled={loading} // Deshabilita el botón durante la carga
                         >
-                            Iniciar Sesión
+                            {loading ? 'Cargando...' : 'Iniciar Sesión'} {/* Cambia el texto del botón durante la carga */}
                         </button>
                     </div>
+                    {error && <p className="text-red-500">{error}</p>}
                 </form>
                 <p className="mt-10 text-center text-sm text-gray-500">
                     ¿No tienes cuenta?{' '}
