cf3065e965ad4d5d804e52475840e893ba3ce664
Jhonatan Becerra <yonkitas9@gmail.com>
2024-02-08 11:38:27 -0500

    fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,12 @@ import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
-import RoutingModule from './routes';
+export const RoutingModule = lazy(() => import('./routes'));
 import { HelmetProvider } from 'react-helmet-async/lib';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HelmetProvider>
     <BrowserRouter>
-      <Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
         <RoutingModule />
       </Suspense>
     </BrowserRouter>
