749ed78593d3d009b7f396a3c15e0dc3a5ece19c
Shinjiomg <yonkitas9@gmail.com>
2024-02-09 11:35:47 -0500

    loading animation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,37 @@
-import React, { Suspense, lazy } from 'react'
+import React, { Suspense, lazy, useState, useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 import { NextUIProvider } from '@nextui-org/react'
-export const RoutingModule = lazy(() => import('./routes'));
+const RoutingModule = lazy(() => import('./routes'));
 import { HelmetProvider } from 'react-helmet-async/lib';
+import LoadingIndicator from './pages/elements/LoadingAnimation';
+
+const delayLoading = () => {
+  return new Promise(resolve => setTimeout(resolve, 2000)); // 2000 milisegundos (2 segundos)
+};
+
+const DelayLoader = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simula la carga demorada
+    delayLoading().then(() => {
+      setIsLoading(false);
+    });
+  }, []);
+  return isLoading ? <LoadingIndicator /> : <RoutingModule />;
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HelmetProvider>
     <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<LoadingIndicator />}>
         <NextUIProvider>
-          <RoutingModule />
+          <DelayLoader />
         </NextUIProvider>
       </Suspense>
     </BrowserRouter>
   </HelmetProvider>
 )
+
--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,41 +1,62 @@
-import {
-    Routes, Route
-} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
-export const RecoverPassword = lazy(() => import('./pages/recover-password'));
-export const UserLogin = lazy(() => import('./pages/user-login'));
-export const UserRegister = lazy(() => import('./pages/user-register'));
-export const ErrorPage = lazy(() => import('./pages/error-page'));
-export const IndexPage = lazy(() => import('./pages/index-page'));
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const RecoverPassword = lazy(async () => {
+    await delay(2000);
+    return import('./pages/recover-password');
+});
+
+const UserLogin = lazy(async () => {
+    await delay(2000);
+    return import('./pages/user-login');
+});
+
+const UserRegister = lazy(async () => {
+    await delay(2000);
+    return import('./pages/user-register');
+});
+
+const ErrorPage = lazy(async () => {
+    await delay(2000);
+    return import('./pages/error-page');
+});
+
+const IndexPage = lazy(async () => {
+    await delay(2000);
+    return import('./pages/index-page');
+});
+import LoadingIndicator from './pages/elements/LoadingAnimation';
 
 
 export default function RoutingModule() {
     return (
         <>
-            <Routes>
-                <Route
-                    path="/"
-                    element={<UserLogin />}
-                />
-                <Route
-                    path="dashboard"
-                    element={<IndexPage />}
-                />
-                <Route
-                    path="register"
-                    element={<UserRegister />}
-                />
+            <Suspense fallback={<LoadingIndicator />}>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<UserLogin />}
+                    />
+                    <Route
+                        path="dashboard"
+                        element={<IndexPage />}
+                    />
+                    <Route
+                        path="register"
+                        element={<UserRegister />}
+                    />
 
-                <Route
-                    path="recover"
-                    element={<RecoverPassword />}
-                />
+                    <Route
+                        path="recover"
+                        element={<RecoverPassword />}
+                    />
 
-                <Route
-                    path="*"
-                    element={<ErrorPage />}
-                />
-            </Routes >
+                    <Route
+                        path="*"
+                        element={<ErrorPage />}
+                    />
+                </Routes >
+            </Suspense>
         </>
     );
 }
