a362cb2ab8f6af3a97b8dd77db0ab6d282c4c0ff
Shinjiomg <yonkitas9@gmail.com>
2024-02-08 20:56:33 -0500

    index

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -6,6 +6,7 @@ export const RecoverPassword = lazy(() => import('./pages/recover-password'));
 export const UserLogin = lazy(() => import('./pages/user-login'));
 export const UserRegister = lazy(() => import('./pages/user-register'));
 export const ErrorPage = lazy(() => import('./pages/error-page'));
+export const IndexPage = lazy(() => import('./pages/index-page'));
 
 
 export default function RoutingModule() {
@@ -14,6 +15,10 @@ export default function RoutingModule() {
             <Routes>
                 <Route
                     path="/"
+                    element={<IndexPage />}
+                />
+                 <Route
+                    path="login"
                     element={<UserLogin />}
                 />
                 <Route
