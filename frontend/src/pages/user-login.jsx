a4dd6e7ae389a6e0e53f80a118dc334847f4326f
Shinjiomg <yonkitas9@gmail.com>
2024-02-08 20:28:59 -0500

    fixes

--- frontend/src/pages/user-login.jsx
+++ frontend/src/pages/user-login.jsx
@@ -2,9 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import app from '../../firebase'
-import { Button } from '@/components/ui/button';
-import { Input } from "@/components/ui/input"
-import { Label } from '@/components/ui/label';
+
 export default function UserLogin() {
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
@@ -37,11 +35,11 @@ export default function UserLogin() {
             <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
                 <form className="space-y-6" method='post' onSubmit={handleSignIn}>
                     <div>
-                        <Label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                             Correo electrónico
-                        </Label>
+                        </label>
                         <div className="mt-2">
-                            <Input
+                            <input
                                 id="email"
                                 name="email"
                                 type="email"
@@ -50,14 +48,15 @@ export default function UserLogin() {
                                 required
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
                     <div>
                         <div className="flex items-center justify-between">
-                            <Label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                                 Contraseña
-                            </Label>
+                            </label>
                             <div className="text-sm">
                                 <Link to="recover" className="font-semibold text-indigo-600 hover:text-indigo-500">
                                     ¿Olvidaste tu contraseña?
@@ -65,7 +64,7 @@ export default function UserLogin() {
                             </div>
                         </div>
                         <div className="mt-2">
-                            <Input
+                            <input
                                 id="password"
                                 name="password"
                                 type="password"
@@ -74,18 +73,19 @@ export default function UserLogin() {
                                 required
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
                     <div>
-                        <Button
+                        <button
                             type="submit"
                             variant="outline"
-                            className="flex w-full justify-center rounded-md"
+                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600"
                             disabled={loading} // Deshabilita el botón durante la carga
                         >
                             {loading ? 'Cargando...' : 'Iniciar Sesión'} {/* Cambia el texto del botón durante la carga */}
-                        </Button>
+                        </button>
                     </div>
                     {error && <p className="text-red-500">{error}</p>}
                 </form>
