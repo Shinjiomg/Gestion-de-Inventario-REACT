8b09da065b9c81bac4fc4ce3c4fb2c778696b74e
Shinjiomg <yonkitas9@gmail.com>
2024-02-19 22:21:47 -0500

    update login

--- frontend/src/pages/user-login.jsx
+++ frontend/src/pages/user-login.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth';
 import { app } from '../../firebase';
-import { Button, Input } from '@nextui-org/react';
+import { Button, Input, Card, CardBody } from '@nextui-org/react';
 import { Link } from 'react-router-dom';
 import LoadingAnimation from './elements/LoadingAnimation'
 
@@ -73,74 +73,78 @@ export default function UserLogin() {
         return <LoadingAnimation />;
     }
     return (
-        <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8 body-login" style={{background: "lightgray"}}>
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm ">
-                <p className="text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                    ¡Bienvenido!
-                </p>
-            </div>
-            <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form className="space-y-6">
-                    <div>
-                        <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-                            <Input
-                                id="email"
-                                name="email"
-                                type="email"
-                                label="Correo electrónico"
-                                autoComplete="email"
-                                isRequired
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </div>
-                    </div>
-                    <div>
-                        <div className="flex items-center justify-end">
-
-                            <div className="mt-5 text-center text-md text-gray-500">
-                                <Link to="/recover" className="font-normal text-blue-600 hover:text-indigo-500"
-                                >
-                                    ¿Olvidaste tu contraseña?
+        <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8 body-login" style={{ background: "#F8F8FF" }}>
+            <div className="sm:mx-auto ">
+                <Card
+                    isBlurred
+                    className="border-none bg-background/60 dark:bg-default-100/50 max-w-[610px]"
+                    shadow="sm"
+                >
+                    <CardBody className="p-8">
+                        <h1 className="text-5xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                            ¡Bienvenido de vuelta!&#128079;
+                        </h1>
+                        <h2 className="text-center text-xl tracking-tight sm:text-xl"
+                        >Inicia sesión con tu cuenta
+                        </h2>
+                        <div className="mt-6 flex flex-col items-center">
+                            <form className="space-y-6 w-full">
+                                <div className="w-full flex flex-wrap md:flex-nowrap gap-4">
+                                    <Input
+                                        id="email"
+                                        name="email"
+                                        type="email"
+                                        label="Correo electrónico"
+                                        autoComplete="email"
+                                        variant="underlined"
+                                        isRequired
+                                        onChange={(e) => setEmail(e.target.value)}
+                                    />
+                                </div>
+                                <div className="w-full flex flex-wrap md:flex-nowrap gap-4">
+                                    <Input
+                                        id="password"
+                                        name="password"
+                                        type="password"
+                                        variant="underlined"
+                                        label="Contraseña"
+                                        autoComplete="current-password"
+                                        isRequired
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                    />
+                                </div>
+                                <div className="flex justify-end">
+                                    <div className="text-center text-md text-gray-500">
+                                        <Link to="/recover" underline="always" className="font-normal text-blue-600 hover:text-indigo-500">
+                                            ¿Olvidaste tu contraseña?
+                                        </Link>
+                                    </div>
+                                </div>
+                                <div>
+                                    <Button
+                                        type="submit"
+                                        onClick={handleSignIn}
+                                        color='primary'
+                                        className="w-full justify-center rounded-md bg-black"
+                                        isLoading={loading}
+                                    >
+                                        {loading ? 'Cargando...' : 'Iniciar Sesión'} {/* Cambia el texto del botón durante la carga */}
+                                    </Button>
+                                </div>
+                                {error && <p className="text-red-500">{error}</p>}
+                            </form>
+                            <p className="mt-5 text-center text-md text-black">
+                                ¿No tienes cuenta?{' '}
+                                <Link className="font-normal text-blue-600 hover:text-indigo-500" underline="always" to="/register">
+                                    Regístrate aquí
                                 </Link>
-                            </div>
+                            </p>
                         </div>
-                        <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-                            <Input
-                                id="password"
-                                name="password"
-                                type="password"
-                                label="Contraseña"
-                                autoComplete="current-password"
-                                isRequired
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                        </div>
-                    </div>
-                    <div>
-                        <Button
-                            type="button"
-                            onClick={handleSignIn}
-                            color='primary'
-                            variant="ghost"
-                            className="flex w-full justify-center rounded-md"
-                            isLoading={loading}
-                        >
-                            {loading ? 'Cargando...' : 'Iniciar Sesión'} {/* Cambia el texto del botón durante la carga */}
-                        </Button>
-                    </div>
-                    {error && <p className="text-red-500">{error}</p>}
-                </form>
-                <p className="mt-5 text-center text-md text-gray-500">
-                    ¿No tienes cuenta?{' '}
-                    <Link className="font-normal text-blue-600 hover:text-indigo-500"
-                        to="/register"
-                    >
-                        Regístrate aquí
-                    </Link>
-                </p>
+                    </CardBody>
+                </Card>
             </div>
-        </div>
+        </div >
     )
 
 }
