5ca8644880d3c2bd43c902e678eef20ab0cc7009
Shinjiomg <yonkitas9@gmail.com>
2024-02-20 21:38:49 -0500

    fixito

--- frontend/src/pages/error-page.jsx
+++ frontend/src/pages/error-page.jsx
@@ -22,13 +22,12 @@ export default function ErrorPage() {
   return (
     <div className="container min-h-screen mx-auto flex flex-col items-center justify-center lg:flex-row">
       <div className="w-full lg:w-1/2 text-center">
-        <p className="text-sm font-medium text-blue-500 dark:text-blue-400">error 404</p>
-        <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">Página no encontrada</h1>
+        <p className="text-lg font-medium text-blue-500 dark:text-blue-400">error 404</p>
+        <h1 className="mt-3 text-2xl font-semibold md:text-3xl">Página no encontrada</h1>
         <p className="mt-4 text-gray-500 dark:text-gray-400">Lo sentimos, la página que estás buscando no existe.</p>
         <p className="mt-5 text-md text-gray-500">
           <Button onClick={handleReturnClick} type="button"
             color='primary'
-            variant="ghost"
             className="mx-auto">
             Volver <span aria-hidden="true">&rarr;</span>
           </Button>
